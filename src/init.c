251fdc98d7e534eee356748d870d2ede86043198
Paul D. McNicholas <mcnicholas@math.mcmaster.ca>
2018-03-14 14:03:13 +0000

    version 1.2.1

---
+++ src/init.c
@@ -0,0 +1,22 @@
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .C calls */
+extern void tchol_entry(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void tcholmeans_entry(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_CMethodDef CEntries[] = {
+    {"tchol_entry",      (DL_FUNC) &tchol_entry,      27},
+    {"tcholmeans_entry", (DL_FUNC) &tcholmeans_entry, 28},
+    {NULL, NULL, 0}
+};
+
+void R_init_longclust(DllInfo *dll)
+{
+    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
